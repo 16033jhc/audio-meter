04026eda058de168de266c91fa571dd165b6c5d0
16033jhc <78768335+16033jhc@users.noreply.github.com>
2022-11-04 14:46:32 +1300

    update volume.py
    more commenting, add tkInter window as exit prompt, write data to text file

--- volume.py
+++ volume.py
@@ -2,6 +2,8 @@
 import pyaudio
 import audioop
 import time
+from tkinter import * 
+from tkinter import messagebox
 
 # Initialise pyaudio and open audio input stream
 p = pyaudio.PyAudio()
@@ -18,14 +20,35 @@ stream = p.open(format=pyaudio_format,
                 input=True,
                 frames_per_buffer=buffer_size)
 
+# Main function
 def main():
+    # Capture audio data
     for i in range(0, int(samplerate / buffer_size)):
         data = stream.read(buffer_size)
         vol = audioop.rms(data, 1)
         time.sleep(2)
 
-        print(f"Volume (in relative DB): {vol}")
+        # Write captured data to text file
+        f = open("sound_history.txt", "a")
+        f.write(f"Volume (in relative DB): {vol}\n")
+        f.close()
 
-while True:
+t_end = time.time() + 60 * 0.001
+
+while time.time() < t_end:
     main()
-    
+
+# Create object 
+root = Tk()
+
+# Adjust size 
+root.geometry("300x200")
+
+messagebox.showinfo("Exiting...", "Thank you!")
+
+# Kill PyAudio streams
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+exit
